ac8604e11db3fd98cf785daf9c6c451694587ef5
Marcus Rodan <marcusrodan@gmail.com>
2017-03-23 20:39:45 +0100

    done l1

--- Lab1/src/lab1/Downloader.java
+++ Lab1/src/lab1/Downloader.java
@@ -2,9 +2,14 @@ package lab1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -30,13 +35,30 @@ public class Downloader {
 	}
 	
 	public static void main(String[] args) throws IOException {
-		/*if(args.length != 2) {
+		if(args.length != 1) {
 			System.err.println("Please enter homepage");
 			System.exit(1);
-		}*/
-		List<URL> pdfFiles = fetchPdfUrls("http://cs229.stanford.edu/materials.html");
-		for(URL url : pdfFiles)
-			System.out.println(url);
+		}
+		List<URL> pdfFiles = fetchPdfUrls(args[0]);
+		download(pdfFiles);
+	}
+
+	private static void download(List<URL> pdfFiles) {
+		Iterator<URL> itr = pdfFiles.iterator();
+		for(int i = 1; i <= pdfFiles.size(); i++) {
+			System.out.println("Downloading: " + i + "/" + pdfFiles.size());
+			download(itr.next());
+		}
+	}
+
+	//http://stackoverflow.com/questions/20265740/how-to-download-a-pdf-from-a-given-url-in-java
+	private static void download(URL file) {
+		try(InputStream in = file.openStream()){
+			String fileName = file.getPath().substring(file.getPath().lastIndexOf('/')+1);
+			Files.copy(in, Paths.get(fileName), StandardCopyOption.REPLACE_EXISTING);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
